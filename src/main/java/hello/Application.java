34f40d4af67737dd36f63df4f43af5c3d3ec6ce4
Darren Lyons <d.lyons88@gmail.com>
2014-03-29 16:35:39 +1100

    Adding more libraries and first spring boot entity/repo

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,6 +1,7 @@
 package hello;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -23,6 +24,42 @@ public class Application {
         for (String beanName : beanNames) {
             System.out.println(beanName);
         }
+
+        UserRepository repository = ctx.getBean(UserRepository.class);
+
+        // save a couple of Users
+        repository.save(new User("Jack", "Bauer"));
+        repository.save(new User("Chloe", "O'Brian"));
+        repository.save(new User("Kim", "Bauer"));
+        repository.save(new User("David", "Palmer"));
+        repository.save(new User("Michelle", "Dessler"));
+
+        // fetch all Users
+        Iterable<User> Users = repository.findAll();
+        System.out.println("Users found with findAll():");
+        System.out.println("-------------------------------");
+        for (User User : Users) {
+            System.out.println(User);
+        }
+        System.out.println();
+
+        // fetch an individual User by ID
+        User User = repository.findOne(1L);
+        System.out.println("User found with findOne(1L):");
+        System.out.println("--------------------------------");
+        System.out.println(User);
+        System.out.println();
+
+        // fetch Users by last name
+        List<User> bauers = repository.findByLastName("Bauer");
+        System.out.println("User found with findByLastName('Bauer'):");
+        System.out.println("--------------------------------------------");
+        for (User bauer : bauers) {
+            System.out.println(bauer);
+        }
+
+        
+        
     }
 
 }
---
+++ src/main/java/hello/UserRepository.java
@@ -0,0 +1,10 @@
+package hello;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    List<User> findByLastName(String lastName);
+}
