eba62c01c11209e5420ed8929d6796ef7117e0a1
Darren Lyons <d.lyons88@gmail.com>
2014-07-12 10:59:23 +1000

    Deleting all code to start again with Maven project template.

--- README.md
+++
@@ -1,4 +0,0 @@
-SocialTransformationWeb
-=======================
-
-Competitive Weight Loss Application Prototype
--- src/main/java/me/darrenlyons/st/OverviewController.java
+++
@@ -1,7 +0,0 @@
-package me.darrenlyons.st;
-
-/**
- * Created by darren on 1/06/14.
- */
-public class OverviewController {
-}
