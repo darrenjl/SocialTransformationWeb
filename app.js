9d7cf6400128c3adb1b90d6d5636dd7f07253760
Darren Lyons <d.lyons88@gmail.com>
2014-07-14 20:03:38 +1000

    Setting up callback urls for server

--- app.js
+++ app.js
@@ -81,9 +81,9 @@ app.use(function(err, req, res, next) {
     });
 });
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080); 
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000); 
 app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
 app.listen(server_port, server_ip_address, function () {
--- config/auth.js
+++ config/auth.js
@@ -1,24 +1,30 @@
 // config/auth.js
 
 // expose our config directly to our application using module.exports
+
+host = 'http://localhost:3000'
+if (process.env.OPENSHIFT_NODEJS_IP) {
+host = 'http://socialtransformation-darrenjl.rhcloud.com'
+}
+
 module.exports = {
 
 	'facebookAuth' : {
 		'clientID' 		: '124346424420251', // your App ID
 		'clientSecret' 	: '87216de18d0c9523dd4f61a8caa7e95f', // your App Secret
-		'callbackURL' 	: 'http://localhost:3000/auth/facebook/callback'
+		'callbackURL' 	: host + '/auth/facebook/callback'
 	},
 
 	'twitterAuth' : {
 		'consumerKey' 		: 'your-consumer-key-here',
 		'consumerSecret' 	: 'your-client-secret-here',
-		'callbackURL' 		: 'http://localhost:3000/auth/twitter/callback'
+		'callbackURL' 		: host + '/auth/twitter/callback'
 	},
 
 	'googleAuth' : {
 		'clientID' 		: '986436769027-urk8u8mhb29diihi027su990qi4hr11e.apps.googleusercontent.com',
 		'clientSecret' 	: 'JQ-0KMnQuJTko8L0U7IdVFOz',
-		'callbackURL' 	: 'http://localhost:3000/auth/google/callback'
+		'callbackURL' 	: host + '/auth/google/callback'
 	}
 
 };
