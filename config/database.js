a4082744d68fa6eb0b3b8dc967013485f8c5b0de
Darren Lyons <d.lyons88@gmail.com>
2014-07-14 19:54:50 +1000

    Updating db_name for openshift config

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 mongodb_connection_string = 'mongodb://localhost/socialtransformation';
 	//take advantage of openshift env vars when available:
 	if(process.env.OPENSHIFT_MONGODB_DB_URL){
-	  mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+	  mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + 'socialtransformation';
 	}
-	
+
 module.exports = {
 	//provide a sensible default for local development
 	
