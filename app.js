84c937aeb8ab3f73974e8e428344f1dd9cc5ebe9
Darren Lyons <d.lyons88@gmail.com>
2014-07-14 19:19:17 +1000

    Adding config for openshift

--- app.js
+++ app.js
@@ -81,5 +81,12 @@ app.use(function(err, req, res, next) {
     });
 });
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
+});
+
 
 module.exports = app;
--- config/database.js
+++ config/database.js
@@ -1,5 +1,12 @@
+mongodb_connection_string = 'mongodb://localhost/socialtransformation';
+	//take advantage of openshift env vars when available:
+	if(process.env.OPENSHIFT_MONGODB_DB_URL){
+	  mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+	}
+	
 module.exports = {
-
-	'url' : 'mongodb://localhost/socialtransformation' 
+	//provide a sensible default for local development
+	
+	'url' : mongodb_connection_string 
 
 };
